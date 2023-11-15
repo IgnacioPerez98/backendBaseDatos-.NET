d1226b0d6a20caf08228f4e236d585c311f4aaea
IgnaciPerez98 <nachopp98@hotmail.com>
2023-11-15 19:17:05 -0300

    probando endpoints

--- backendBaseDatos/Controllers/UCUAdminController.cs
+++ backendBaseDatos/Controllers/UCUAdminController.cs
@@ -49,7 +49,7 @@ namespace backendBaseDatos.Controllers
         [SwaggerResponse(StatusCodes.Status400BadRequest, Description = "Error en la validacion del periodo")]
         [SwaggerResponse(StatusCodes.Status401Unauthorized, Description = "Error en el token proporcionado")]
         [SwaggerResponse(StatusCodes.Status500InternalServerError, Description = "Error del servidor")]
-        [Authorize]
+        //[Authorize]
         public IActionResult ObtenerFuncionariosdesactualizados()
         {
             try
