3d46e2a9c0108d229fce3f0c0d2e15116abc3271
IgnacioPerez98 <nachopp98@hotmail.com>
2023-11-25 12:52:57 -0300

    add swagger

--- backendBaseDatos/Program.cs
+++ backendBaseDatos/Program.cs
@@ -70,13 +70,8 @@ namespace backendBaseDatos
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
